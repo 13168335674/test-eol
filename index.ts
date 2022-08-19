288fef0fa97e8444b334efa65875b263f3a415b1
adi <569643843@qq.com>
2022-08-19 10:29:46 +0800

    000002

--- index.ts
+++ index.ts
@@ -1,6 +1,6 @@
 console.log('ADI-LOG => 111111');
 console.log('ADI-LOG => 111111');
 console.log('ADI-LOG => 111111');
-console.log('ADI-LOG => 000000');
+console.log('ADI-LOG => 000002');
 console.log('ADI-LOG => 111111');
 console.log('ADI-LOG => 111111');
